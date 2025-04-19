12956372c9a97e2a8c065ed70f43a05339da0d5c
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2025-04-19 17:48:27 +0530

    Create mode-toggle.tsx

---
+++ app/components/mode-toggle.tsx
@@ -0,0 +1,34 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+import { useTheme } from "./theme-provider";
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleToggle = () => {
+    console.log(theme, "theme");
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={handleToggle}>
+      {/* Show Sun icon for dark mode, Moon for light mode */}
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
