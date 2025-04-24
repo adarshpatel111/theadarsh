babcaaab4b317c19a45c622dea5bc1ed9d246a3f
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2025-04-24 12:34:03 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("projects", "routes/projects.tsx"),
-  route("about", "routes/about.tsx"),
   route("resume", "routes/resume.tsx"),
   route("contact-me", "routes/contact-me.tsx"),
 ] satisfies RouteConfig;
