dd8e6e402c1d73c7714df1132832c5cf17225992
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2025-04-26 15:00:54 +0530

    Update projects.tsx

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import ProjectCards from "~/components/Projects/project-cards";
 
-const projects = () => {
-  return <div>projects projects</div>;
-}
+const Projects: React.FC = () => {
+  return (
+    <div className="container w-full mx-auto mt-35">
+      <header className="w-full px-4 sm:px-6 lg:px-8 mb-16 flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-5xl text-center font-bold text-primary mb-4">
+          My Projects
+        </h1>
+      </header>
+      <ProjectCards />
+    </div>
+  );
+};
 
-export default projects
+export default Projects;
