40765b71ff159b93de0cd1c5b93652a9cad7742b
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2025-04-26 15:00:16 +0530

    Update Navbar.tsx

--- app/components/rootComponents/Navbar.tsx
+++ app/components/rootComponents/Navbar.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import {
-  Home,
-  CircleUser,
-  BookOpen,
-  AtSign,
-  LayoutDashboard,
-  Menu,
-} from "lucide-react";
+import { Home, BookOpen, AtSign, LayoutDashboard, Menu } from "lucide-react";
 import { MorphingNav } from "../ui/devsloka-ui/MorphingNav";
 import { Button } from "~/components/ui/button";
+
+import { motion } from "framer-motion";
 import {
   Sheet,
   SheetClose,
@@ -19,12 +14,11 @@ import {
 } from "~/components/ui/sheet";
 import { Link, useLocation } from "react-router";
 
-const Navbar : React.FC= () => {
+const Navbar: React.FC = () => {
   const pathRoute = useLocation();
   const navItems = [
     { id: "/", label: "Home", icon: Home, href: "/" },
     { id: "/resume", label: "Resume", icon: BookOpen, href: "/resume" },
-    // { id: "/about", label: "About", icon: CircleUser, href: "/about" },
     {
       id: "/projects",
       label: "Projects",
@@ -41,7 +35,42 @@ const Navbar : React.FC= () => {
   return (
     <div className="container flex justify-center align-ceneter mx-auto">
       <div className="container flex justify-between items-center bg-background/70 backdrop-blur-lg fixed top-6 z-50 py-4 px-10 border border-dashed rounded-xl shadow-md ">
-        <div className="logo">TheAdarsh</div>
+        <div className="flex items-center space-x-3">
+          <div className="relative w-10 h-10">
+            <motion.div
+              className="absolute inset-0 rounded-lg bg-primary/20"
+              animate={{
+                scale: [1, 1.2, 1],
+                rotate: [0, 90, 0],
+                borderRadius: ["20%", "50%", "20%"],
+              }}
+              transition={{
+                duration: 8,
+                repeat: Number.POSITIVE_INFINITY,
+                repeatType: "loop",
+              }}
+            />
+            <motion.div
+              className="absolute inset-2 rounded-md bg-primary"
+              animate={{
+                rotate: [0, 180, 0],
+                scale: [1, 0.8, 1],
+              }}
+              transition={{
+                duration: 8,
+                repeat: Number.POSITIVE_INFINITY,
+                repeatType: "loop",
+                delay: 0.5,
+              }}
+            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl gradient-text">
+              A
+            </div>
+          </div>
+          <span className="text-2xl font-semibold logo-span gradient-text">
+            TheAdarsh
+          </span>
+        </div>
 
         <MorphingNav
           items={navItems}
